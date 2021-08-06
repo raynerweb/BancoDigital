bfbdd6a2c9f7e7f0b4a9a463d2d6ef005a0d924e
rayner <raynerweb@gmail.com>
2021-08-06 13:45:11 -0300

    add telas de fluxo de pagamento

--- BancoDigital/View/TransferViewController.swift
+++ BancoDigital/View/TransferViewController.swift
@@ -13,6 +13,31 @@ class TransferViewController: UIViewController {
     let viewModel: TransferViewModel
     let disposeBag = DisposeBag()
     
+    var constraintTop: CGFloat?
+    var constraintBottom: CGFloat?
+    var constraintLeft: CGFloat?
+    var constraintRight: CGFloat?
+
+    
+    @IBAction func favored(_ sender: Any) {
+        if let segmentedControl = sender as? UISegmentedControl {
+            let index = segmentedControl.selectedSegmentIndex
+            if index == 0 {
+                self.hideDocumentField()
+            } else {
+                self.showDocumentField()
+            }
+        }
+        
+    }
+    
+
+    @IBOutlet weak var favoredDocumentConstraintLeft: NSLayoutConstraint!
+    @IBOutlet weak var favoredDocumentConstraintRight: NSLayoutConstraint!
+    @IBOutlet weak var favoredDocumentContraintBottom: NSLayoutConstraint!
+    @IBOutlet weak var favoredDocumentoConstraintTop: NSLayoutConstraint!
+    @IBOutlet weak var favoredDocument: UIStackView!
+    
     @IBOutlet weak var selectedBank: UIButton!
     @IBAction func selectBank(_ sender: Any) {
         viewModel.showBankList()
@@ -34,6 +59,14 @@ class TransferViewController: UIViewController {
         subscribe()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        self.constraintTop = self.favoredDocumentoConstraintTop.constant
+        self.constraintBottom = self.favoredDocumentContraintBottom.constant
+        self.constraintRight = self.favoredDocumentConstraintRight.constant
+        self.constraintLeft = self.favoredDocumentConstraintLeft.constant
+        self.hideDocumentField()
+    }
+    
     private func subscribe() {
         viewModel.selectedBankState.subscribe { event in
             if let bank = event.element, let name = bank?.name {
@@ -42,7 +75,29 @@ class TransferViewController: UIViewController {
         }.disposed(by: disposeBag)
     }
     
-
+    private func hideDocumentField() {
+        self.favoredDocumentoConstraintTop.constant = 0
+        self.favoredDocumentContraintBottom.constant = 0
+        self.favoredDocumentConstraintLeft.constant = 0
+        self.favoredDocumentConstraintRight.constant = 0
+        self.favoredDocument.isHidden = true
+        self.view.layoutIfNeeded()
+    }
+    
+    private func showDocumentField() {
+        
+        self.favoredDocumentoConstraintTop.constant = self.constraintTop ?? 0.0
+        
+        self.favoredDocumentContraintBottom.constant = self.constraintBottom ?? 0.0
+        
+        self.favoredDocumentConstraintLeft.constant = self.constraintLeft ?? 0.0
+        
+        self.favoredDocumentConstraintRight.constant = self.constraintRight ?? 0.0
+        
+        self.favoredDocument.isHidden = false
+        self.view.layoutIfNeeded()
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ BancoDigital/View/ValueTransferViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ValueTransferViewController.swift
+//  BancoDigital
+//
+//  Created by rayner on 05/08/21.
+//
+
+import UIKit
+
+class ValueTransferViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
